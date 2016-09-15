2276f80a0a7ee7a5e0d8ab10dfdeb1340ec29510
yudin-alexey <alexeyvet@gmail.com>
2016-09-15 06:59:51 +0400

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>hMusic</title>
+    </head>
+    <body>
+	<h1>hMusic</h1>
+	<p>Under construction</p>
+    </body>
+</html>
