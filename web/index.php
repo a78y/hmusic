16ac9ac50a543b4eedd1a65196c38cc5e2ac212d
Алексей Юдин <alexeyvet@gmail.com>
2016-09-20 11:07:22 +0300

    Refactoring

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use H\Music;
 
 $configuration = new Music\ApplicationConfiguration();
-$configuration->load(dirname(__DIR__) . '/resources/config/global.yaml');
+$configuration->load(dirname(__DIR__) . '/resources/config/application.yaml');
 
 $app = new Music\Application($configuration);
 $app->run();
