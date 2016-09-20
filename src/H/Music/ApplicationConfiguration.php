8d4e975e81ed1bafc430740d587303f9fcd9bbd4
Алексей Юдин <alexeyvet@gmail.com>
2016-09-20 11:03:23 +0300

    Refactoring

--- src/H/Music/ApplicationConfiguration.php
+++ src/H/Music/ApplicationConfiguration.php
@@ -9,28 +9,22 @@ class ApplicationConfiguration implements ApplicationConfigurationInterface
 	private $parser;
 	private $configuration;
 
-	// class constructor
     public function __construct()
     {
 		$this->parser = new Yaml();
 		$this->configuration = array();
     }
 
-	// load configuration
     public function load($fileName) 
     {
-    	// check exists configuration file
 		if (file_exists($fileName)) 
 		{
-			// load configuration file
 			if ($configuration = $this->parser->parse($fileName)) 
 			{
 				$this->configuration = array_merge_recursive($this->configuration, $configuration);
 
-				// load extended configurations
 				if (!empty($configuration['extends'])) 
 				{
-					// set string to array
 					if (!is_array($configuration['extends'])) 
 					{
 						$configuration['extends'] = array($configuration['extends']);
