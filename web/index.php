57242546553adb1fedf1da6c97a189d998e0b66c
yudin-alexey <alexeyvet@gmail.com>
2016-09-27 08:59:27 +0400

    Refactoring

--- web/index.php
+++ web/index.php
@@ -2,9 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use H\Music\Application;
-
-$app = new Application();
-$app->register(new Silex\Provider\ServiceControllerServiceProvider());
-$app->register(new DF\Silex\Provider\YamlConfigServiceProvider(dirname(__DIR__) . '/resources/config/application.yaml'));
+$app = new H\Music\Application(dirname(__DIR__));
 $app->run();
