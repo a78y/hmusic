917956a82097209b574c380a06a1f2a317fb644d
Алексей Юдин <alexeyvet@gmail.com>
2016-09-30 18:24:23 +0300

    Remove debug info

--- web/index.php
+++ web/index.php
@@ -2,8 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$class = 'H\Music\Controller\BandController';
-$instance = new $class();
-
 $app = new H\Music\Application(dirname(__DIR__));
 $app->run();
